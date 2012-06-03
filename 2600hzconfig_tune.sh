1f2e7d8dacde1ba2e53de8434e44bafc76f6562f
mx8 <mx8@mail.ru>
2012-06-03 14:08:15 +0400

    1

--- 2600hzconfig_tune.sh
+++
@@ -1,8 +0,0 @@
-sed -i "s/apps.2600hz.com/94.125.0.113/g" /var/www/html/config/config.js
-sed -i "s/company_name: '2600hz'/company_name: 'OnNet'/g" /var/www/html/config/config.js
-sed -i "s/login: '.sip.2600hz.com'/login: '.sip2.onnet.su'/g" /var/www/html/config/config.js
-sed -i "s/register: '.trial.2600hz.com'/register: '.sip2.onnet.su'/g" /var/www/html/config/config.js
-sed -i "s/url: ''/url: 'http:\/\/94.125.0.113:8000\/v1'/g" /var/www/html/config/config.js
-sed -i "s/apps001-demo-ord.2600hz.com/94.125.0.113/g" /var/www/html/config/config.js
-sed -i "s/www.2600hz.org\/support.html/helpdesk.telepark.info/g" /var/www/html/config/config.js
-#sed -i "s///g" /var/www/html/config/config.js
